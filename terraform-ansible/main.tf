a0b118a230860ba75199306a640b37fccc22ce34
macloudbykumar <81429447+macloudbykumar@users.noreply.github.com>
2025-07-24 16:03:52 +0530

    Update main.tf

--- terraform-ansible/main.tf
+++ terraform-ansible/main.tf
@@ -23,13 +23,13 @@ terraform {
 
 
 resource "google_compute_network" "vpc" {
-  name                    = "centos9-vpc"
+  name                    = "centos9-vpc1"
   auto_create_subnetworks = true
 }
 
 /*
 resource "google_compute_instance" "centos9_vm" {
-  name         = "centos9-vm"
+  name         = "centos9-vm1"
   machine_type = "e2-medium"
   zone = "us-central1-a"
   boot_disk {
@@ -56,7 +56,7 @@ output "instance_ip" {
 */
 
 resource "google_compute_instance" "centos9_vm" {
-  name         = "centos9-vm"
+  name         = "centos9-vm1"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
@@ -77,7 +77,7 @@ resource "google_compute_instance" "centos9_vm" {
       #!/bin/bash
       useradd -m -s /bin/bash devops
       mkdir -p /home/devops/.ssh
-      echo "ssh-rsa AAAAB3Nza... your-new-user-public-key ..." > /home/devops/.ssh/authorized_keys
+      echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBsHIxxTGG1KDFVHB3020cQcViPE72XjfIgfPchK9xJHpXmSWsh9YcmXeuZdV7le7Dm2B0srqusUmwc5UzJFt1EeG3xw1SrxGfFziimhPV8xhox3Y9aft2h31lTkQg9lFoPJ8RU6vxgNr6xkRdgRD1GXj7imVZu9XKcJXcQI8DI9PY/nEuLh7k4SZ4gERNqvZjoouZzMLBkhISDOlnPR46a9SwkvM7Rm4ob1W/B8Zoo8hOPvD58JtDyaZd4d0mZmE6R1GolAyFJKhn1x4sUSYhXc9cv/P4+3HKp1I01aFKzqKiC3jD0SUDNHTSVfDJ84NneBsmKdR05NrLdDp37yYLBZZsKlZ9jHXA16ioQwNs6FsaCDb2QsQz67WqUTX7jTuyR3EV8lVq5nkljo15UcDPqPuE9k3t1fTGbBuEY0J+Sa6Z9EpQwfDaqKDlIHyXpgEdhmqpdzQXgm4HcvjgogiZiJdpQcpxZRKLCMxns8NLGpkl70HRCRB/SrxZPnb9KdWbYekssI4/CMUcbtQRjDTvDjH3WnavMKVhJ1bMIYaVQGJ/gZ4XxTCRNafweHKU13SdyM0mRlZ1mgAGIvUtpzk0gt9NWUnTWZm7BbT0MbcpadRyDjDFozNMxEAgFbq+S0Gf31YCWyJTFd5FA4dOgEdSgXOmN9aT07yvHIaYYIeewQ== eduarnofficial@gcpvm-github-terraform-ansible-centos9" > /home/devops/.ssh/authorized_keys
       chown -R devops:devops /home/devops/.ssh
       chmod 700 /home/devops/.ssh
       chmod 600 /home/devops/.ssh/authorized_keys
