058a8e96480d9a144be240836e6981d67509bad1
macloudbykumar <81429447+macloudbykumar@users.noreply.github.com>
2025-07-24 11:59:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,11 @@ provider "google" {
   project     = var.project_id
   region      = var.region
 }
+
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state-prod-bykumar"
+    prefix  = "terraform/state"
+  }
+}
