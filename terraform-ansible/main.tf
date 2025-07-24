5ed82a4ac26ce109c0631adb8b7fbd1fcaa95385
macloudbykumar <81429447+macloudbykumar@users.noreply.github.com>
2025-07-24 17:08:42 +0530

    Update main.tf

--- terraform-ansible/main.tf
+++ terraform-ansible/main.tf
@@ -73,9 +73,6 @@ resource "google_compute_instance" "centos9_vm" {
       chown -R devops:devops /home/devops/.ssh
       chmod 700 /home/devops/.ssh
       chmod 600 /home/devops/.ssh/authorized_keys
-      usermod -aG wheel devops
-      echo "devops ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/devops
-      chmod 440 /etc/sudoers.d/devops
     EOT
   }
 
