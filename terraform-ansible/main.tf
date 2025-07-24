3cbec2c5cf0b706bd9f76418d6baf24fffcbeb5a
macloudbykumar <81429447+macloudbykumar@users.noreply.github.com>
2025-07-24 16:27:00 +0530

    Update main.tf

--- terraform-ansible/main.tf
+++ terraform-ansible/main.tf
@@ -64,9 +64,9 @@ resource "google_compute_firewall" "allow_ssh" {
   name    = "allow-ssh"
   network = google_compute_network.vpc.name
 
-  allows {
+  allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["22", "80", "8080", "1000-2000"]
   }
 
   direction = "INGRESS"
